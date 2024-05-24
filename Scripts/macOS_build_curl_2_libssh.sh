87dd958a168ffb37c9c3bc98210f4ecdb96f2e23
Nicholas Orr <nick@goya.com.au>
2024-05-24 14:35:43 +1000

    Working curl build

---
+++ Scripts/macOS_build_curl_2_libssh.sh
@@ -0,0 +1,46 @@
+#!/bin/bash -E
+
+cd ../Output
+export OUTPUT=`pwd`
+
+# Remove old libraries
+
+rm Libraries/macOS/libssh2.a
+rm -rf Headers/libssh2
+mkdir Headers/libssh2
+
+# Starting folder
+
+cd ../source/macOS
+export SRCROOT=`pwd`
+
+# Switch to our build directory
+
+rm -rf libssh
+mkdir libssh
+tar -xf ../libssh.tar.gz -C libssh --strip-components=1
+cd libssh
+
+mkdir _build_macos
+mkdir _build_ios
+mkdir _build_iosSimulator
+mkdir _build_iosSimulatorArm
+mkdir _build_iosSimulatorx86
+
+export PREFIX=`pwd`'/_build_macos'
+export PREFIX_ios=`pwd`'/_build_ios'
+export PREFIX_iosSimulator=`pwd`'/_build_iosSimulator'
+export PREFIX_iosSimulatorArm=`pwd`'/_build_iosSimulatorArm'
+export PREFIX_iosSimulatorx86=`pwd`'/_build_iosSimulatorx86'
+
+# Build macOS
+
+CFLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=10.15 -I${OUTPUT}/Headers -I${OUTPUT}/Headers/openssl" LDFLAGS="-L${OUTPUT}/Libraries/macOS/" LIBS="-ldl" ./configure --disable-shared --disable-examples-build --prefix="${PREFIX}" -exec-prefix="${PREFIX}" --with-libz --with-crypto=openssl
+make -s -j install
+
+# Copy the header and library files.
+
+cp -R "${PREFIX}/include/*" "${OUTPUT}/Headers/libssh2"
+cp "${PREFIX}/lib/libssh2.a" "${OUTPUT}/Libraries/macOS"
+
+cd ${SRCROOT}

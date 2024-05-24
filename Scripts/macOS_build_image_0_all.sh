70f7edf1e87e5f964e47ade0ae98d0c07cb8b1ed
Nicholas Orr <nick@goya.com.au>
2024-05-24 11:26:31 +1000

    Removed libjpeg

--- Scripts/macOS_build_image_0_all.sh
+++ Scripts/macOS_build_image_0_all.sh
@@ -1,11 +1,7 @@
 #!/bin/bash -E
 
 ./macOS_build_image_1_libturbojpeg.sh
-
-#Not working yet, only produces x86 builds
 ./macOS_build_image_2_libde265.sh
-
-./macOS_build_image_3_libjpeg.sh
-./macOS_build_image_4_libheif.sh
-./macOS_build_image_5_libopenjp2.sh
-./macOS_build_image_6_imagemagik.sh
+./macOS_build_image_3_libheif.sh
+./macOS_build_image_4_libopenjp2.sh
+./macOS_build_image_5_imagemagik.sh

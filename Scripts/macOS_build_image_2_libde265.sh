4455b8e068605ceb290dc805222e57bdaa339429
Nicholas Orr <nick@goya.com.au>
2024-05-24 10:38:21 +1000

    Got the libde stuff compiling at last

--- Scripts/macOS_build_image_2_libde265.sh
+++ Scripts/macOS_build_image_2_libde265.sh
@@ -30,26 +30,13 @@ export PREFIX_iosSimulatorx86=`pwd`+'/_build_iosSimulatorx86'
 
 # Build macOS
 
-#cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_C_FLAGS="-arch x86_64 -arch arm64 -mmacosx-version-min=10.15" -DBUILD_SHARED_LIBS:BOOL=OFF -DENABLE_SDL:BOOL=FALSE -DENABLE_SHERLOCK265:BOOL=FALSE -DENABLE_DECODER:BOOL=FALSE -DCMAKE_OSX_DEPLOYMENT_TARGET:STRING="10.13" ./
-
-autoupdate
-autoreconf -fi
-./autogen.sh
-
-./configure --host arm64-apple-darwin --prefix="${PREFIX}" CFLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=10.15" --disable-arm --disable-shared --enable-static --disable-dec265 --disable-sherlock265 --disable-sse --disable-dependency-tracking
-
-#./configure CC="gcc -arch arm64 -arch x86_64" \
-#            CXX="g++ -arch arm64 -arch x86_64" \
-#            CPP="gcc -E" CXXCPP="g++ -E"
-
-#./configure CC="gcc -arch x86_64 -arch arm64" CXX="g++ -arch x86_64 -arch arm64" CPP="gcc -E" CXXCPP="g++ -E"
-
-#make -s -j install
+cmake -DCMAKE_BUILD_TYPE=RELEASE -DCMAKE_INSTALL_PREFIX="${PREFIX}" -DCMAKE_OSX_ARCHITECTURES="arm64;x86_64" -DBUILD_SHARED_LIBS=OFF -DENABLE_SDL=FALSE -DENABLE_SHERLOCK265=FALSE -DENABLE_DECODER=OFF -DCMAKE_OSX_DEPLOYMENT_TARGET="10.15" ./
+make -s -j install
 
 # Copy the header and library files.
 
-#cp -R "${PREFIX}/include/libde265" "${OUTPUT}/Headers/libde265"
-#cp "${PREFIX}/lib/libde265.a" "${OUTPUT}/Libraries/macOS"
+cp -R "${PREFIX}/include/libde265" "${OUTPUT}/Headers/libde265"
+cp "${PREFIX}/lib/libde265.a" "${OUTPUT}/Libraries/macOS"
 
 # Return to source directory
 

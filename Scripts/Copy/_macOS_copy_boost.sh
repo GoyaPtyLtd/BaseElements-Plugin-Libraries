1ef27700e1e353c6bbdc041b9b7e769f2e2792aa
Nicholas Orr <nick@goya.com.au>
2024-06-14 16:27:40 +1000

    Added copy

---
+++ Scripts/Copy/_macOS_copy_boost.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd ../../..
+
+export START=`pwd`
+
+cd BaseElements-Plugin-Libraries/Output
+
+export OUTPUT=`pwd`
+
+cp Libraries/macOS/libboost_atomic.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libboost_date_time.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libboost_filesystem.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libboost_program_options.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libboost_regex.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libboost_thread.a "${START}/BaseElements-Plugin/Libraries/macOS"
+
+rsync -av --delete --checksum "${OUTPUT}/Headers/boost" "${START}/BaseElements-Plugin/Headers/boost"

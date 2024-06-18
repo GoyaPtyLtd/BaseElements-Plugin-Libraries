85addad807e3eecdb8f00521307b4a98732e4a01
Nicholas Orr <nick@goya.com.au>
2024-06-18 13:24:53 +1000

    Initial commit, untested
    Can be put into the main macOS build script when working

---
+++ Scripts/iOS_build_libturbojpeg.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+# iPhone version
+
+IOS_PLATFORMDIR=$(xcrun --sdk iphoneos --show-sdk-platform-path)
+IOS_SYSROOT=$(xcrun --sdk iphoneos --show-sdk-path)
+
+export CFLAGS="-Wall -arch arm64 -miphoneos-version-min=13.2 -funwind-tables"
+
+cat <<EOF >toolchain.cmake
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_C_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang)
+EOF
+
+cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
+  -DCMAKE_OSX_SYSROOT=${IOS_SYSROOT[0]} \
+  .
+make
+
+#mv -v ./libturbojpeg.a ../libs/libturbojpeg_arm64_a
+
+make clean
+
+# Simulator version
+
+IOS_SIMULATOR_PLATFORMDIR=$(xcrun --sdk iphonesimulator --show-sdk-platform-path)
+IOS_SIMULATOR_SYSROOT=$(xcrun --sdk iphonesimulator --show-sdk-path)
+
+export CFLAGS="-Wall -arch x86_64 arm64 -miphoneos-version-min=13.2 -funwind-tables"
+
+cat <<EOF >toolchain.cmake
+set(CMAKE_SYSTEM_NAME Darwin)
+set(CMAKE_SYSTEM_PROCESSOR aarch64)
+set(CMAKE_C_COMPILER /Applications/Xcode.app/Contents/Developer/Toolchains/XcodeDefault.xctoolchain/usr/bin/clang)
+EOF
+
+cmake -G"Unix Makefiles" -DCMAKE_TOOLCHAIN_FILE=toolchain.cmake \
+  -DCMAKE_OSX_SYSROOT=${IOS_SIMULATOR_SYSROOT[0]} \
+  .
+make
+
+#mv -v ./libturbojpeg.a ../libs/libturbojpeg_arm64_a
+
+make clean

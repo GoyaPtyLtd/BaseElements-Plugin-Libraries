06c4b7a6061f951d61b57743dd242dea4c030abf
Nicholas Orr <nick@goya.com.au>
2024-07-02 22:37:38 +1000

    Create Copy script for font librarie

---
+++ Scripts/Copy/_macOS_copy_font.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cd ../../..
+
+export START=`pwd`
+
+cd BaseElements-Plugin-Libraries/Output
+
+export OUTPUT=`pwd`
+
+cp Libraries/macOS/libfreetype.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libfontconfig.a "${START}/BaseElements-Plugin/Libraries/macOS"
+cp Libraries/macOS/libpodofo.a "${START}/BaseElements-Plugin/Libraries/macOS"
+
+rsync -rv --delete --no-group --no-owner --no-perms --no-times --checksum  --stats "${OUTPUT}/Headers/freetype2/" "${START}/BaseElements-Plugin/Headers/freetype2"
+
+rsync -rv --delete --no-group --no-owner --no-perms --no-times --checksum  --stats "${OUTPUT}/Headers/fontconfig/" "${START}/BaseElements-Plugin/Headers/fontconfig"
+
+rsync -rv --delete --no-group --no-owner --no-perms --no-times --checksum  --stats "${OUTPUT}/Headers/podofo/" "${START}/BaseElements-Plugin/Headers/podofo"

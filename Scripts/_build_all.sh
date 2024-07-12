2796e675ca0ec57323f83c9520141a0e55699202
Nicholas Orr <nick@goya.com.au>
2024-07-12 15:15:13 +1000

    Update _build_all.sh

--- Scripts/_build_all.sh
+++ Scripts/_build_all.sh
@@ -9,5 +9,4 @@ set -e
 ./build_image_0_all.sh
 ./build_xml_0_all.sh
 
-# the podofo library at the end of the font group has dependencies on both xml and openssl
-#./macOS_build_font_0_all.sh
+./macOS_build_font_0_all.sh

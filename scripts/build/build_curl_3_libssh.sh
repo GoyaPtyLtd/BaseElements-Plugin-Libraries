d2983af8f3ecf06859c3e680ffb5357f4638967e
Nicholas Orr <nick@goya.com.au>
2024-07-16 10:49:41 +1000

    build libssh fix path to header files

--- scripts/build/build_curl_3_libssh.sh
+++ scripts/build/build_curl_3_libssh.sh
@@ -99,9 +99,9 @@ fi
 # Copy the header and library files.
 
 if [ ${PLATFORM} = 'macOS' ]; then
-	cp -R _build_x86_64/include/libssh2/* "${OUTPUT}/Headers/libssh2"
+	cp -R _build_x86_64/include/* "${OUTPUT}/Headers/libssh2"
 else
-	cp -R _build/include/libssh2/* "${OUTPUT}/Headers/libssh2"
+	cp -R _build/include/* "${OUTPUT}/Headers/libssh2"
 fi
 
 cp _build/lib/libssh2.a "${OUTPUT}/Libraries/${PLATFORM}"

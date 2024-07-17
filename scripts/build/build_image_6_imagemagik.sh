51c97cae3ebcf3d8c2edb1ad38aa2a5a2cd9647b
Nicholas Orr <nick@goya.com.au>
2024-07-17 18:31:06 +1000

    Add fPic to IM

--- scripts/build/build_image_6_imagemagik.sh
+++ scripts/build/build_image_6_imagemagik.sh
@@ -92,6 +92,7 @@ if [ ${PLATFORM} = 'macOS' ]; then
 
 elif [ ${PLATFORM} = 'linux' ]||[ ${PLATFORM} = 'linuxARM' ]; then
 
+	CFLAGS="-fPIC" \
 	./configure --disable-shared --disable-docs --disable-dependency-tracking \
 	--with-heic=yes --with-freetype=yes --with-fontconfig=yes --with-png=yes --with-jpeg=yes --with-openjp2=yes \
 	--without-utilities --without-zlib --without-xml --without-lzma --with-quantum-depth=16 \

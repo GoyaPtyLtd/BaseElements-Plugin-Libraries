9985abfd5f13c44f6f3e2dc15746796d0ac291f0
Gavin Stewart <gav-@users.noreply.github.com>
2024-08-02 16:42:38 +1000

    Update build_boost.sh

--- scripts/build/build_boost.sh
+++ scripts/build/build_boost.sh
@@ -3,33 +3,50 @@ set -e
 
 echo "Starting $(basename "$0") Build"
 
-if [ $(uname) = 'Darwin' ]; then
-	export PLATFORM='macOS'
-elif [ $(uname -m) = 'aarch64' ]; then
-	export PLATFORM='linuxARM'
-else
-	export PLATFORM='linux'
+OS=$(uname -s)		# Linux|Darwin
+ARCH=$(uname -m)	# x86_64|aarch64|arm64
+JOBS=1              # Number of parallel jobs
+if [[ $OS = 'Darwin' ]]; then
+	PLATFORM='macOS'
+    JOBS=$(($(sysctl -n hw.logicalcpu) + 1))
+elif [[ $OS = 'Linux' ]]; then
+    JOBS=$(($(nproc) + 1))
+    if [[ $ARCH = 'aarch64' ]]; then
+        PLATFORM='linuxARM'
+    elif [[ $ARCH = 'x86_64' ]]; then
+        PLATFORM='linux'
+    fi
+fi
+if [[ "${PLATFORM}X" = 'X' ]]; then     # $PLATFORM is empty
+	echo "!! Unknown OS/ARCH: $OS/$ARCH"
+	exit 1
 fi
 
-export SRCROOT=`pwd`
+SRCROOT=$(pwd)
 cd ../../Output
-export OUTPUT=`pwd`
+OUTPUT=$(pwd)
 
 # Remove old libraries and headers
 
-rm -f Libraries/${PLATFORM}/libboost_atomic.a
-rm -f Libraries/${PLATFORM}/libboost_date_time.a
-rm -f Libraries/${PLATFORM}/libboost_filesystem.a
-rm -f Libraries/${PLATFORM}/libboost_program_options.a
-rm -f Libraries/${PLATFORM}/libboost_regex.a
-rm -f Libraries/${PLATFORM}/libboost_thread.a
+LIBS=(
+    libboost_atomic.a
+    libboost_date_time.a
+    libboost_filesystem.a
+    libboost_program_options.a
+    libboost_regex.a
+    libboost_thread.a
+)
+
+for LIB in "${LIBS[@]}"; do
+    rm -f Libraries/"${PLATFORM}"/"${LIB}"
+done
 
 rm -rf Headers/boost
 mkdir Headers/boost
 
 # Switch to our build directory
 
-cd ../source/${PLATFORM}
+cd ../source/"${PLATFORM}"
 
 rm -rf boost
 mkdir boost
@@ -37,40 +54,46 @@ tar -xf ../boost.tar.gz -C boost --strip-components=1
 cd boost
 
 mkdir _build
-export PREFIX=`pwd`'/_build'
+PREFIX=$(pwd)/_build
 
 # Build
 
 ./bootstrap.sh
 
-if [ ${PLATFORM} = 'macOS' ]; then
-
-	./b2 toolset=clang cxxflags="-arch arm64 -arch x86_64" \
-	address-model=64 link=static runtime-link=static install \
-	--prefix="${PREFIX}" \
-	--with-program_options --with-regex --with-date_time --with-filesystem --with-thread \
-	cxxflags="-mmacosx-version-min=10.15 -stdlib=libc++" linkflags="-stdlib=libc++"
-
-elif [ ${PLATFORM} = 'linux' ]||[ ${PLATFORM} = 'linuxARM' ]; then
-
-	./b2 cflags=-fPIC cxxflags=-fPIC \
-	address-model=64 link=static runtime-link=static install \
-	-j$(($(nproc) + 1)) \
-	--prefix="${PREFIX}" \
-	--with-program_options --with-regex --with-date_time --with-filesystem --with-thread
-
+CXXFLAGS=()
+LINKFLAGS=()
+if [[ $PLATFORM = 'macOS' ]]; then
+    CXXFLAGS+=(
+        -arch arm64
+        -arch x86_64
+        '-mmacosx-version-min=10.15'
+        '-stdlib=libc++'
+    )
+    LINKFLAGS+=(
+        '-stdlib=libc++'
+    )
+elif [[ $PLATFORM = 'linux' ]]; then
+    CXXFLAGS+=(
+        -fPIC
+    )
 fi
+
+./b2 toolset=clang \
+    cxxflags="${CXXFLAGS[*]}" \
+    linkflags="${LINKFLAGS[*]}" \
+    address-model=64 link=static runtime-link=static \
+    --with-program_options --with-regex --with-date_time \
+    --with-filesystem --with-thread \
+    --prefix="${PREFIX}" -j${JOBS} \
+    install
 
 # Copy the header and library files.
 
 cp -R _build/include/boost/* "${OUTPUT}/Headers/boost"
 
-cp _build/lib/libboost_atomic.a "${OUTPUT}/Libraries/${PLATFORM}"
-cp _build/lib/libboost_date_time.a "${OUTPUT}/Libraries/${PLATFORM}"
-cp _build/lib/libboost_filesystem.a "${OUTPUT}/Libraries/${PLATFORM}"
-cp _build/lib/libboost_program_options.a "${OUTPUT}/Libraries/${PLATFORM}"
-cp _build/lib/libboost_regex.a "${OUTPUT}/Libraries/${PLATFORM}"
-cp _build/lib/libboost_thread.a "${OUTPUT}/Libraries/${PLATFORM}"
+for LIB in "${LIBS[@]}"; do
+    cp _build/lib/"${LIB}" "${OUTPUT}/Libraries/${PLATFORM}"
+done
 
 #Build iOS
 
@@ -80,4 +103,4 @@ cp _build/lib/libboost_thread.a "${OUTPUT}/Libraries/${PLATFORM}"
 
 # Return to source directory
 
-cd ${SRCROOT}
+cd "${SRCROOT}"

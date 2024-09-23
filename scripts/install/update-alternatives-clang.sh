8a13bd0ac2a5e192d3d64f2354071ea8cf4bbcb9
Nicholas Orr <nick@goya.com.au>
2024-09-23 13:13:40 +1000

    Update update-alternatives-clang.sh

--- scripts/install/update-alternatives-clang.sh
+++ scripts/install/update-alternatives-clang.sh
@@ -16,7 +16,6 @@ function register_clang_version {
         --slave   /usr/bin/llvm-extract      llvm-extract     /usr/bin/llvm-extract-${version} \
         --slave   /usr/bin/llvm-link         llvm-link        /usr/bin/llvm-link-${version} \
         --slave   /usr/bin/llvm-mc           llvm-mc          /usr/bin/llvm-mc-${version} \
-#        --slave   /usr/bin/llvm-mcmarkup     llvm-mcmarkup    /usr/bin/llvm-mcmarkup-${version} \
         --slave   /usr/bin/llvm-nm           llvm-nm          /usr/bin/llvm-nm-${version} \
         --slave   /usr/bin/llvm-objdump      llvm-objdump     /usr/bin/llvm-objdump-${version} \
         --slave   /usr/bin/llvm-ranlib       llvm-ranlib      /usr/bin/llvm-ranlib-${version} \
@@ -27,14 +26,19 @@ function register_clang_version {
         --slave   /usr/bin/llvm-symbolizer   llvm-symbolizer  /usr/bin/llvm-symbolizer-${version} \
         --slave   /usr/bin/llvm-tblgen       llvm-tblgen      /usr/bin/llvm-tblgen-${version}
 
+#        --slave   /usr/bin/llvm-mcmarkup     llvm-mcmarkup    /usr/bin/llvm-mcmarkup-${version} \
+
     update-alternatives \
         --install /usr/bin/clang                 clang                 /usr/bin/clang-${version} ${priority} \
         --slave   /usr/bin/clang++               clang++               /usr/bin/clang++-${version}  \
         --slave   /usr/bin/asan_symbolize        asan_symbolize        /usr/bin/asan_symbolize-${version} \
+        --slave   /usr/bin/clang-cpp             clang-cpp             /usr/bin/clang-cpp-${version} \
+        --slave   /usr/bin/lldb                  lldb                  /usr/bin/lldb-${version} \
+        --slave   /usr/bin/lldb-server           lldb-server           /usr/bin/lldb-server-${version}
+
 #        --slave   /usr/bin/c-index-test          c-index-test          /usr/bin/c-index-test-${version} \
 #        --slave   /usr/bin/clang-check           clang-check           /usr/bin/clang-check-${version} \
 #        --slave   /usr/bin/clang-cl              clang-cl              /usr/bin/clang-cl-${version} \
-        --slave   /usr/bin/clang-cpp             clang-cpp             /usr/bin/clang-cpp-${version} \
 #        --slave   /usr/bin/clang-format          clang-format          /usr/bin/clang-format-${version} \
 #        --slave   /usr/bin/clang-format-diff     clang-format-diff     /usr/bin/clang-format-diff-${version} \
 #        --slave   /usr/bin/clang-import-test     clang-import-test     /usr/bin/clang-import-test-${version} \
@@ -44,8 +48,7 @@ function register_clang_version {
 #        --slave   /usr/bin/clang-rename          clang-rename          /usr/bin/clang-rename-${version} \
 #        --slave   /usr/bin/clang-reorder-fields  clang-reorder-fields  /usr/bin/clang-reorder-fields-${version} \
 #        --slave   /usr/bin/clang-tidy            clang-tidy            /usr/bin/clang-tidy-${version} \
-        --slave   /usr/bin/lldb                  lldb                  /usr/bin/lldb-${version} \
-        --slave   /usr/bin/lldb-server           lldb-server           /usr/bin/lldb-server-${version}
+
 }
 
 register_clang_version ${version} ${priority}

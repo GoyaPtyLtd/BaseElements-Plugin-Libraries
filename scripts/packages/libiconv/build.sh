2e7e1b3c644441d194dd7fe8aa871f3b8c850bdb
Gavin Stewart <gav-@users.noreply.github.com>
2024-10-10 22:57:43 +1100

    Migrate libiconv and libunistring to new build system

---
+++ scripts/packages/libiconv/build.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# --- BOILERPLATE ---
+# Determine this script's directory.
+REALDIR=$(dirname "$(realpath "$0")")
+cd "${REALDIR}" || exit 1
+# --- BOILERPLATE ---
+
+# Build package.
+#
+# Almost every package will need to define a build() function.
+#
+# Only a virtual package does not define a build() function. It is just used
+# as a way to group DEPENDENCIES in the "package" file.
+#
+# Uses global variables:
+#   PACKAGE_SRC      - from fetch()
+#   PLATFORM
+#   PLATFORM_INCLUDE
+#   PLATFORM_LIBS
+#   PLATFORM_FRAMEWORKS
+#   BUILD_LOG
+build() {
+    cd "${PACKAGE_SRC}" || exit 1
+
+    rm -rf _build
+    mkdir _build
+    local PREFIX=${PWD}'/_build'
+
+    # Build
+    rm -f "${BUILD_LOG}"
+    print_ok "Building ..."
+    (
+
+        if [[ $PLATFORM =~ ^macos ]]; then
+
+            CFLAGS="-arch arm64 -arch x86_64 -mmacosx-version-min=10.15" \
+            ./configure --disable-shared --prefix="${PREFIX}"
+
+        elif [[ $PLATFORM =~ ^ubuntu ]]; then
+
+            CC=clang CXX=clang++ \
+            CFLAGS=-fPIC \
+            ./configure --disable-shared --prefix="${PREFIX}"
+
+        fi
+
+        make -j"${JOBS}"
+        make install
+
+    ) >> "${BUILD_LOG}" 2>&1 &
+    wait_progress $!
+    return_code=$?
+    if [[ $return_code -ne 0 ]]; then
+        print_error "Build failed. Return code: $return_code"
+        exit 1
+    fi
+
+    print_ok "Build complete."
+
+
+    # Copy the header and library files.
+
+    cp -R _build/include/* "${PLATFORM_INCLUDE}/"
+
+    cp _build/lib/libiconv.a "${PLATFORM_LIBS}/"
+    cp _build/lib/libcharset.a "${PLATFORM_LIBS}/"
+
+}
+
+# --- BOILERPLATE ---
+# Source this after required functions are defined.
+source ../build-functions
+# --- BOILERPLATE ---

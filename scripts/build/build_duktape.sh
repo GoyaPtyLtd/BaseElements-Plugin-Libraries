363f5d84e2c9a46adb07b5d122adf14a342b08e2
DanSmith888 <dan@mustardfire.com>
2025-11-05 20:45:53 +0800

    refactored duktape build script

--- scripts/build/build_duktape.sh
+++ scripts/build/build_duktape.sh
@@ -1,45 +1,47 @@
 #!/bin/bash
 set -e
 
-echo "Starting $(basename "$0") Build"
+# Source common build functionality (platform detection, paths, interactive mode, colors, helpers)
+# This allows the script to be run standalone. When called from 2_build.sh,
+# variables are already exported, but sourcing again is harmless.
+source "$(dirname "$0")/_build_common.sh" "$@"
 
-OS=$(uname -s)		# Linux|Darwin
-ARCH=$(uname -m)	# x86_64|aarch64|arm64
-JOBS=1              # Number of parallel jobs
-if [[ $OS = 'Darwin' ]]; then
-	PLATFORM='macOS'
-    JOBS=$(($(sysctl -n hw.logicalcpu) + 1))
-elif [[ $OS = 'Linux' ]]; then
-    JOBS=$(($(nproc) + 1))
-    if [[ $ARCH = 'aarch64' ]]; then
-        PLATFORM='linuxARM'
-    elif [[ $ARCH = 'x86_64' ]]; then
-        PLATFORM='linux'
-    fi
-fi
-if [[ "${PLATFORM}X" = 'X' ]]; then     # $PLATFORM is empty
-	echo "!! Unknown OS/ARCH: $OS/$ARCH"
-	exit 1
-fi
+LIBRARY_NAME="duktape"
+ARCHIVE_NAME="duktape.tar.xz"
+
+print_header "Starting ${LIBRARY_NAME} Build"
+
+# Clean and create output directories (ensures they exist and are empty)
+interactive_prompt \
+    "Ready to clean and create output directories for ${LIBRARY_NAME}" \
+    "Will remove and recreate: ${OUTPUT_INCLUDE}/${LIBRARY_NAME}" \
+    "Will remove and recreate: ${OUTPUT_LIB}/${LIBRARY_NAME}" \
+    "Will remove and recreate: ${OUTPUT_SRC}/${LIBRARY_NAME}"
+
+rm -rf "${OUTPUT_INCLUDE}/${LIBRARY_NAME}"
+rm -rf "${OUTPUT_LIB}/${LIBRARY_NAME}"
+rm -rf "${OUTPUT_SRC}/${LIBRARY_NAME}"
+
+mkdir -p "${OUTPUT_INCLUDE}/${LIBRARY_NAME}"
+mkdir -p "${OUTPUT_LIB}/${LIBRARY_NAME}"
+mkdir -p "${OUTPUT_SRC}/${LIBRARY_NAME}"
+
+# Extract source to output/platforms/${PLATFORM}/src/
+interactive_prompt \
+    "Ready to extract source archive" \
+    "Archive: ${SOURCE_ARCHIVES}/${ARCHIVE_NAME}" \
+    "Destination: ${OUTPUT_SRC}/${LIBRARY_NAME}"
+
+cd "${OUTPUT_SRC}/${LIBRARY_NAME}"
+tar -xf "${SOURCE_ARCHIVES}/${ARCHIVE_NAME}" --strip-components=1
+
+# Copy source files (duktape is header-only, no compilation needed)
+interactive_prompt \
+    "Ready to copy source files for ${LIBRARY_NAME}" \
+    "Source: ${OUTPUT_SRC}/${LIBRARY_NAME}/src/" \
+    "Destination: ${OUTPUT_INCLUDE}/${LIBRARY_NAME}/"
+
+cp -R ${OUTPUT_SRC}/${LIBRARY_NAME}/src/* "${OUTPUT_INCLUDE}/${LIBRARY_NAME}/"
 
 
-SRCROOT=${PWD}
-cd ../../Output
-OUTPUT=${PWD}
-
-# Switch to our build directory
-
-cd ../source/${PLATFORM}
-
-rm -rf duktape
-mkdir duktape
-tar -xf ../duktape.tar.xz -C duktape --strip-components=1
-cd duktape
-
-# Copy the source files.
-
-cp -R src "${OUTPUT}/Source/duktape"
-
-# Return to source directory
-
-cd "${SRCROOT}"
+print_success "Build complete for ${LIBRARY_NAME}"

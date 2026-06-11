f24f2dc469316e652d6615c18a0c383fa02e138a
Nicholas Orr <nick@goya.com.au>
2026-06-12 08:40:33 +1000

    Modify ImageMagick to cp all .a files instead of our fixed 3.

--- scripts/build/build_image_6_imagemagik.sh
+++ scripts/build/build_image_6_imagemagik.sh
@@ -242,9 +242,14 @@ cp -R "${PREFIX}/include/ImageMagick-7"/* "${OUTPUT_INCLUDE}/${LIBRARY_NAME}/" 2
 
 #Nothing else needs the .a so we don't need to copy them.
 if [[ $OS = 'Windows' ]]; then
-    convert_to_lib "${PREFIX}/lib/libMagick++-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/Magick++-7.Q16HDRI.lib"
-    convert_to_lib "${PREFIX}/lib/libMagickCore-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/MagickCore-7.Q16HDRI.lib"
-    convert_to_lib "${PREFIX}/lib/libMagickWand-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/MagickWand-7.Q16HDRI.lib"
+# Windows builds a heap of lib files, so we copy them all as is.
+#    convert_to_lib "${PREFIX}/lib/libMagick++-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/Magick++-7.Q16HDRI.lib"
+#    convert_to_lib "${PREFIX}/lib/libMagickCore-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/MagickCore-7.Q16HDRI.lib"
+#    convert_to_lib "${PREFIX}/lib/libMagickWand-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/MagickWand-7.Q16HDRI.lib"
+    for $file in "${PREFIX}/lib/"*.a; do
+		filename=$(basename "$file")
+		convert_to_lib "$file" "${OUTPUT_LIB}/${LIBRARY_NAME}/${filename/%.a/.lib}"
+    done
 else
 	cp "${PREFIX}/lib/libMagick++-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/"
 	cp "${PREFIX}/lib/libMagickCore-7.Q16HDRI.a" "${OUTPUT_LIB}/${LIBRARY_NAME}/"
